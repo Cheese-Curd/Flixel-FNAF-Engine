fb36f07bf8d12a433fac2be668ae2499f95cda7e
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2023-02-02 18:17:13 -0500

    QoL Changes to Uitl

--- source/DebugUtils.hx
+++ source/DebugUtils.hx
@@ -7,27 +7,37 @@ class DebugUtils
 {
 	public static function debugPos(thing:FlxObject)
 	{
-		if (FlxG.keys.pressed.UP)
+		if (FlxG.keys.pressed.SHIFT)
 		{
-			thing.y--;
+			if (FlxG.keys.pressed.UP)
+				thing.y--;
+			if (FlxG.keys.pressed.DOWN)
+				thing.y++;
+			// x \\
+			if (FlxG.keys.pressed.RIGHT)
+				thing.x++;
+			if (FlxG.keys.pressed.LEFT)
+				thing.x--;
 		}
-		if (FlxG.keys.pressed.DOWN)
+		else
 		{
-			thing.y++;
-		}
-		// x \\
-		if (FlxG.keys.pressed.RIGHT)
-		{
-			thing.x++;
-		}
-		if (FlxG.keys.pressed.LEFT)
-		{
-			thing.x--;
+			if (FlxG.keys.justReleased.UP)
+				thing.y--;
+			if (FlxG.keys.justReleased.DOWN)
+				thing.y++;
+			// x \\
+			if (FlxG.keys.justReleased.RIGHT)
+				thing.x++;
+			if (FlxG.keys.justReleased.LEFT)
+				thing.x--;
 		}
 		if (FlxG.keys.justReleased.ENTER)
 		{
 			trace('[DEBUG] X: ' + thing.x);
 			trace('[DEBUG] Y: ' + thing.y);
+
+			FlxG.log.add('[DEBUG] X: ' + thing.x);
+			FlxG.log.add('[DEBUG] Y: ' + thing.y);
 		}
 	}
 }
